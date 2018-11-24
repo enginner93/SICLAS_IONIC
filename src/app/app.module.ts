ad1cfbb97593aaf566bd14986f897170f45bcc80
enginner93 <jesusasallas@hotmail.com>
2018-11-24 14:59:21 -0500

    Mi Apps de Ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { FormularioPage } from '../pages/formulario/formulario';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +14,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    FormularioPage,
   ],
   imports: [
     BrowserModule,
@@ -23,7 +24,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    FormularioPage
   ],
   providers: [
     StatusBar,
